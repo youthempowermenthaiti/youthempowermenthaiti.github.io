889466884b88267a328358f7d12ea3a6492df0ea
Heracraft <heracraftgame.ne@gmail.com>
2023-06-21 17:25:19 +0300

    donate intersection observer

--- javascript/buttonjs.js
+++ javascript/buttonjs.js
@@ -1,18 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var firstButton = document.querySelector(".cosulting.hvr-vertical");
-    var secondButton = document.getElementById("none");
-  
-    window.addEventListener("scroll", function() {
-      var windowHeight = window.innerHeight;
-      var scrollTop = window.pageYOffset;
-      var firstButtonTop = firstButton.offsetTop;
-  
-      if (scrollTop > firstButtonTop + firstButton.offsetHeight - windowHeight) {
-        // console.log("ifeeee");
-        secondButton.classList.remove("none");
-      } else {
-        console.log("aea");
-        secondButton.classList.add("none");
-      }
-    });
-  });
+const donateButton = document.querySelector(".cosulting.hvr-vertical");
+const root = document.querySelector(".top-bar")
+const floatingDonateButton=document.querySelector("#none")
+
+let observer=new IntersectionObserver((enteries)=>{
+  // console.log(enteries[0].isIntersecting);
+  floatingDonateButton.classList.toggle("active")
+})
+
+observer.observe(donateButton)
